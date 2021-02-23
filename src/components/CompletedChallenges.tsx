5b76145879aa855fa613179142cdf7df67c76a70
rafaelwaterkemper <rafaelwaterkemper@gmail.com>
2021-02-23 20:50:06 -0300

    Finished second day :rocket:

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,11 @@
+import { syncBuiltinESMExports } from 'node:module'
+import styles from '../styles/components/CompletedChallenges.module.css'
+
+export function CompletedChallenges() {
+    return (
+        <div className={styles.completedChallengesContainer}>
+            <span>Desafios completos</span>
+            <span>5</span>
+        </div>
+    )
+}
